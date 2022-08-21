902e1d1a3b41faafe712c6261c1095fcbcffcd9b
shimahi <shimahi@reah.jp>
2022-08-22 00:57:05 +0900

    setup

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'capacitor_practice',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
