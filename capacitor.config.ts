29ed1fa73bf0fbfe54f3bfb1ac4307a0f45c4c0f
shimahi <shimahi@reah.jp>
2022-08-22 23:13:37 +0900

    diff

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,18 +7,17 @@ const config: CapacitorConfig = {
   bundledWebRuntime: true,
   /**
    * @description
-   * ローカルでネイティブの検証を行う際は、live reloadをするために、自分の環境でのIPアドレス・ポートを server に設定する
+   * ローカル環境でネイティブの検証を行う際は、live reloadをするために、自分の環境でのIPアドレス・ポートを server に設定しコメントアウトを解除すること
    * この差分はgitに残さないこと
    * https://capacitorjs.com/docs/guides/live-reload
    *
    * 環境変数参照できるようにしたい
    * https://github.com/ionic-team/capacitor/discussions/5204
    */
-  server: {
-    // url: 'http://[あなたのネット環境のIPアドレス]:[webアプリのローカルサーバーのポート(Next.jsのデフォルトでは3000)]'
-    url: 'http://192.168.150.191:3000',
-    cleartext: true,
-  },
+  // server: {
+  //   url: 'http://[あなたのネット環境のIPアドレス]:[webアプリのローカルサーバーのポート(Next.jsのデフォルトでは3000)]',
+  //   cleartext: true,
+  // },
 }
 
 export default config
